b3a52e444f8ea38084719029348b95fa197aef35
Frieder Bluemle <frieder.bluemle@gmail.com>
2019-11-11 14:12:27 -0800

    Fix formatting, whitespace, markdownlint

--- wait.js
+++ wait.js
@@ -1,11 +1,10 @@
-let wait = function(milliseconds) {
-  return new Promise((resolve, reject) => {
-    if (typeof(milliseconds) !== 'number') { 
-      throw new Error('milleseconds not a number'); 
+let wait = function (milliseconds) {
+  return new Promise((resolve) => {
+    if (typeof milliseconds !== 'number') {
+      throw new Error('milliseconds not a number');
     }
-
     setTimeout(() => resolve("done!"), milliseconds)
   });
-}
+};
 
 module.exports = wait;
