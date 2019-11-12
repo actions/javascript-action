f103191ee8bc392bc3b79ed768d7bc7f56b21556
homoluctus <w.slife18sy@gmail.com>
2019-11-13 03:50:42 +0900

    [utils] Create isAllowedStatus function

--- src/utils.ts
+++ src/utils.ts
@@ -1,36 +1,14 @@
 /**
- * Convert status string to number
- * Return 1 (Status.Success) if argument word contains "success"
- * Return 0 (Status.Failure) if argument word contains "fail"
- * @param status {string}
+ * Check if status entered by user is allowed by GitHub Actions.
+ * @param {string} status - job status
+ * @returns {string}
  */
-export function getStatus(status: string): Status {
-  const lowercase_status: string = status.toLowerCase();
+export function isAllowedStatus(status: string): string {
+  const lowercaseStatus: string = status.toLowerCase();
+  const jobStatusList: string[] = ['success', 'failure', 'cancelled'];
 
-  if (lowercase_status.includes('success')) {
-    return Status.Success;
-  } else if (lowercase_status.includes('fail')) {
-    return Status.Failure;
-  } else if (lowercase_status.includes('cancel')) {
-    return Status.Cancel;
-  } else if (lowercase_status === 'always') {
-    return Status.Always;
-  } else {
-    throw new Error(`
-      The argument includes an unacceptable word.
-      "type" parameter allows to include "success", "fail" or "cancel".
-      "mention_if" parameter can include "always" in addition to the above.
-    `);
+  if (!jobStatusList.includes(lowercaseStatus)) {
+    throw new Error('Invalid value input');
   }
-}
-
-/**
- * Job status
- */
-export enum Status {
-  Failure = 0,
-  Success = 1,
-  Cancel = 2,
-  Always = 3,
-  None = 100
+  return status;
 }
