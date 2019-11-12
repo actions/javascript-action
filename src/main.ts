bf56552a205e33b09fcfb40754f718e12f439426
homoluctus <w.slife18sy@gmail.com>
2019-11-13 03:53:13 +0900

    [main] Integrate from kebab-case to camel-case and remove no need code

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import * as core from '@actions/core';
 import { Slack } from './slack';
-import { Status, getStatus } from './utils';
+import { isAllowedStatus } from './utils';
 
 async function run() {
   try {
-    const status: Status = getStatus(core.getInput('type', { required: true }));
-    const job_name: string = core.getInput('job_name', { required: true });
+    const status: string = isAllowedStatus(core.getInput('type', { required: true }));
+    const jobName: string = core.getInput('job_name', { required: true });
     const mention: string = core.getInput('mention');
-    const tmp_mention_if: string = core.getInput('mention_if');
+    const mentionCondition: string = core.getInput('mention_if');
     const username: string = core.getInput('username');
-    const icon_emoji: string = core.getInput('icon_emoji');
+    const iconEmoji: string = core.getInput('icon_emoji');
     const channel: string = core.getInput('channel');
     const url: string = core.getInput('url') || process.env.SLACK_WEBHOOK || '';
 
@@ -21,15 +21,8 @@ async function run() {
       `);
     }
 
-    let mention_if: Status;
-    if (tmp_mention_if === '') {
-      mention_if = Status.None;
-    } else {
-      mention_if = getStatus(tmp_mention_if);
-    }
-
-    const slack = new Slack(url, username, icon_emoji, channel);
-    const payload = slack.generatePayload(job_name, mention, mention_if, status);
+    const slack = new Slack(url, username, iconEmoji, channel);
+    const payload = slack.generatePayload(jobName, mention, mentionCondition, status);
     const result = await slack.notify(payload);
 
     core.debug(`Response from Slack: ${JSON.stringify(result)}`);
