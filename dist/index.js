834d9da3bdb498c3345cde9ec1766b9e58d0c5da
Dominik Maszczyk <Skitionek@gmail.com>
2020-04-24 15:52:51 +0200

    fix: response error?

--- dist/index.js
+++ dist/index.js
@@ -1249,7 +1249,7 @@ class MSTeams {
 		const client = new IncomingWebhook(url);
 		const response = await client.send(payload);
 
-		if (response.text !== '1') {
+		if (response.text !== 1) {
 			throw new Error(
 				"Failed to send notification to Microsoft Teams.\n" +
 				"Response:\n" +
--- src/MSTeams.js
+++ src/MSTeams.js
@@ -201,7 +201,7 @@ class MSTeams {
 		const client = new IncomingWebhook(url);
 		const response = await client.send(payload);
 
-		if (response.text !== '1') {
+		if (response.text !== 1) {
 			throw new Error(
 				"Failed to send notification to Microsoft Teams.\n" +
 				"Response:\n" +
