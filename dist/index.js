27fc4391b19d06afd790108ba8beb37581ab3821
CoderGears <support@codergears.com>
2021-01-19 11:34:54 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -7,18 +7,28 @@ require('./sourcemap-register.js');module.exports =
 
 const core = __nccwpck_require__(186);
 const wait = __nccwpck_require__(258);
-
+const octokit = new Octokit();
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const octokit = new Octokit();
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+// See https://developer.github.com/v3/issues/#create-an-issue
+const { data } = await octokit.request("Get /repos/{owner}/{repo}/contents/license", {
+  owner,
+  repo
+  
+});
+core.info(`content: ${data.content}`);
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds2 ...`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
-
+    var tempDirectory = process.env['RUNNER_TEMP'] ;
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const core = require('@actions/core');
 const wait = require('./wait');
-
+const octokit = new Octokit();
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const octokit = new Octokit();
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+// See https://developer.github.com/v3/issues/#create-an-issue
+const { data } = await octokit.request("Get /repos/{owner}/{repo}/contents/license", {
+  owner,
+  repo
+  
+});
+core.info(`content: ${data.content}`);
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds2 ...`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
-
+    var tempDirectory = process.env['RUNNER_TEMP'] ;
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
